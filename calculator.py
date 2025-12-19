a0afd8aaf1d4f553795e61191940be3d6d028ab0
zejnoz <privetznakomtes@gmail.com>
2025-12-19 21:51:44 +0400

    Add main execution block

--- calculator.py
+++ calculator.py
@@ -14,3 +14,10 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero!")
     return a / b
+
+
+if __name__ == "__main__":
+    print(add(2, 2))
+    print(sub(10, 8))
+    print(multiply(52, 10))
+    print(divide(10, 7))
