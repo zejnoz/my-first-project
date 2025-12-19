405eb03183bebb510b89ce997384569f6d6a1c2f
zejnoz <privetznakomtes@gmail.com>
2025-12-19 21:50:14 +0400

    Add multiplication and division functions

--- calculator.py
+++ calculator.py
@@ -4,3 +4,13 @@ def add(a, b):
 
 def sub(a, b):
     return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero!")
+    return a / b
